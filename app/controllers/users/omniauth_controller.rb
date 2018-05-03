ee761afc22fdf0480da9e735f6ee30a3b8a6ced1
prathap442 <pmohansaikrishna@gmail.com>
2018-05-03 15:10:45 +0530

    removing the binding.pry and trying to display the messages

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -53,14 +53,12 @@ class Users::OmniauthController < ApplicationController
     end
 
     def google_oauth2
-    	binding.pry
-		@user = User.create_from_provider_data(request.env['omniauth.auth'])
-		binding.pry
+    	@user = User.create_from_provider_data(request.env['omniauth.auth'])
 		if @user.persisted?
 		  sign_in_and_redirect @user
 		  flash[:success] = "Welcome to the Rails app you are authenticated! with Google" if is_navigational_format?
 		else
-		  flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+		  flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later. This is due to #{@user.errors.full_messages}'
 		  redirect_to new_user_registration_url
 		end 
     end
